8cddeab7bf43c90dde3847d02d3033fa125f0a02
deg32 <deg32@mail.ru>
2016-12-06 16:19:22 +0700

    заменен @task на @shared_task

--- blog_app/tasks.py
+++ blog_app/tasks.py
@@ -1,9 +1,10 @@
-from celery import task, Celery
+from celery import task, shared_task
 from datetime import datetime
 from django.core.mail import send_mail
 
 
-@task
+# логирование добавления нового комментария
+@shared_task(name='comments_log')
 def comment_add_log(comment_author):
 
     with open('log.txt', 'a') as f:
@@ -11,7 +12,8 @@ def comment_add_log(comment_author):
         f.write(comment_author + ' add comment at ' + datetime.strftime(datetime.today(), "%H:%M %d.%m.%Y") + '\n')
 
 
-@task
+#рассылка писем при добавлении нового комментария
+@shared_task
 def send_mail_to_subscribers(message):
 
     send_mail('New comment', 'New comment text:' + message, 'from@example.com',['to@example.com'], fail_silently = False)
